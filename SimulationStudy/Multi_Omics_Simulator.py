4a39dfe5ed8f5db2236c90c89b447e935320b129
david-hirst <david.hirst@univ-amu.fr>
2023-10-10 15:54:19 +0200

    Initial commit

---
+++ SimulationStudy/Multi_Omics_Simulator.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 11:05:07 2022
+
+@author: david
+"""
+
+# This is a function for simulating multi-omics data from latent factors 
+# loosely based on the generative model from the MOFA paper
+# various paramaters and hyperparamters can be changed
+# A score matrix and M weight matrices are generated with guassian distirbutions
+# Binary matrices are generated for each omics - elementwise multiplication with guassian distributed weight matrices produces final weight matrices
+# The product of each weight matrix and the score matrix produces a matrix of parameter values from which the observed data is simulated based on specified likelihood
+
+
+import numpy as np
+import sys
+
+## define function for simulating a multi-omics dataset
+   
+def SimMultiOmics(CLrn = 20, CLrnNPool = [10,25,40], CTrg = 2, CTrgN = 7,
+                  K = 30, ZMuBase = 5, ZSdRange = [1,1], ZMuShift = [-2,2], ZShiftP = 1/4,
+                  D = [1000,1000,1000,1000], YDists = ["Poisson", "Normal", "Beta", "Bernoulli"], 
+                  WThetaRange = [0.1, 0.2],
+                  WMuPois = 5, WSdRangePois = [0.5, 1.5],
+                  WMuNorm = 0, WSdRangeNorm = [0.5, 1.5],
+                  WMuBeta = 0, WSdRangeBeta = [0.25, 0.75], 
+                  WMuBern = 0, WSdRangeBern = [0.5, 1.5],
+                  YSdRangeNorm = [0.25, 0.75], YaRangeBeta = [50, 150]):
+    
+    ### Explanation of parameters
+    # CLrn: number of learning dataset sample groups
+    # CLrnNPool: pool of sizes for learning dataset sample groups
+    # CTrg: number of target dataset sample groups
+    # CTrgN: size of each target dataset sample group
+    # K: number of latent factors
+    # ZMuBase: Base mean parameter for factor sample scores
+    # ZSdRange: range from which to choose standard deviation for sample scores for each factor
+    # ZMuShift: potential shifts in mean parameter for sample factor scores
+    # ZShiftP: probability that score mean parameter shifts for a particular sample group and factor
+    # D: number of features for each omic
+    # YDists: observed data likelihoods for each omic
+    # WThetaRange: parameter for generating bernoulli random variables to induce sparsity in weight matrices
+    # WMuPois, WMuNorm, etc : the mean parameter for simulating weight matrices depending on the corresponding observed data likelihood
+    # WSdRangePois, WSdRangeNorm, etc : the standard deviation parameter for simulating weight matrices depending on the corresponding observed data likelihood
+    # YSdRangeNorm: range from which to select featurewise standard deviations for generating guassian observed data
+    # YaRangeBeta: range from which to select featurewise alpha parameter values when generating beta distributed observed data
+    
+
+    ########## Check feature counts and distributions
+    
+    # number of views
+    M = len(D)
+    
+    if M != len(YDists) :
+        print("The vectors D and YDists must be the same length")
+        sys.exit()
+     
+    for m in range(M): 
+        if YDists[m]!="Poisson" and YDists[m]!="Normal" and YDists[m]!="Beta" and YDists[m]!="Bernoulli":
+            print("Distributions must be either Poisson, Normal, Beta or Bernoulli")
+            sys.exit()
+            
+             
+    ########## Create Sample clusters (groups)   
+     
+    # combined learning and target sets
+    C = CLrn + CTrg
+    Nc = np.append(np.random.choice(a = CLrnNPool, size = CLrn),np.repeat(CTrgN,CTrg))
+    # assign sample groupings
+    CIds = np.repeat(a = range(C), repeats = Nc)
+    IfLrn = CIds <= max(range(CLrn))
+    
+    
+    ######### Simulate score values
+    
+    # pool base and shift values
+    ZMuPool = np.append(ZMuBase,ZMuBase+np.array(ZMuShift))
+    # sampling probabilities for the pooled values
+    ZSampP = np.append(1-ZShiftP, np.repeat(ZShiftP/len(ZMuShift),len(ZMuShift)))
+    
+    # empty lists for parameter values and sampled scores
+    ZMuC = []
+    ZMuS = []
+    Z = []
+    
+    # cluster means
+    for k in range(K):
+        ZMuC.append(np.random.choice(a = ZMuPool, size = C, p = ZSampP))
+        
+    # sample means based on corresponding cluster value
+    for k in range(K):
+        ZMuS.append(np.repeat(a = ZMuC[k], repeats = Nc))
+        
+    # generate scores for each sample
+    for k in range(K):
+        Z.append(np.random.normal(loc = ZMuS[k], 
+                                  scale = np.random.uniform(low=ZSdRange[0], high=ZSdRange[1], size=1), 
+                                  size=len(ZMuS[k])))
+          
+    ZMuC = np.vstack(ZMuC).transpose()   
+    Z = np.vstack(Z).transpose()
+    
+    ######### Simulate weight matrices
+    
+    W = []        
+    
+    for m in range(M):
+        if YDists[m] == "Poisson":
+            W.append(
+                np.column_stack(
+                    [np.random.normal(loc = WMuPois, 
+                                      scale = np.random.uniform(low=WSdRangePois[0], high=WSdRangePois[1], size=1), 
+                                      size = D[m]) * 
+                     np.random.binomial(n = 1, 
+                                        p = np.random.uniform(low=WThetaRange[0], high=WThetaRange[1], size=1), 
+                                        size = D[m]) 
+                     for k in range(K)]
+                    )
+                )
+        elif YDists[m] == "Normal":
+            W.append(
+                np.column_stack(
+                    [np.random.normal(loc = WMuNorm, 
+                                      scale = np.random.uniform(low=WSdRangeNorm[0], high=WSdRangeNorm[1], size=1), 
+                                      size = D[m]) * 
+                     np.random.binomial(n = 1, 
+                                        p = np.random.uniform(low=WThetaRange[0], high=WThetaRange[1], size=1), 
+                                        size = D[m]) 
+                     for k in range(K)]
+                    )
+                )
+        elif YDists[m] == "Beta":
+            W.append(
+                np.column_stack(
+                    [np.random.normal(loc = WMuBeta, 
+                                      scale = np.random.uniform(low=WSdRangeBeta[0], high=WSdRangeBeta[1], size=1), 
+                                      size = D[m]) * 
+                     np.random.binomial(n = 1, 
+                                        p = np.random.uniform(low=WThetaRange[0], high=WThetaRange[1], size=1), 
+                                        size = D[m]) 
+                     for k in range(K)]
+                    )
+                )
+        elif YDists[m] == "Bernoulli":
+            W.append(
+                np.column_stack(
+                    [np.random.normal(loc = WMuBern, 
+                                      scale = np.random.uniform(low=WSdRangeBern[0], high=WSdRangeBern[1], size=1), 
+                                      size = D[m]) * 
+                     np.random.binomial(n = 1, 
+                                        p = np.random.uniform(low=WThetaRange[0], high=WThetaRange[1], size=1), 
+                                        size = D[m]) 
+                     for k in range(K)]
+                    )
+                )
+        else: 
+            print("Distributions must be either Poisson, Normal, Beta or Bernoulli")
+            sys.exit()
+            
+    ##### Simulate Data
+        
+    ## Calculate signal
+    
+    S = []
+    for m in range(M):
+        S.append(Z @ W[m].T)
+        
+    ## Use the signal to simulate the data
+    
+    Y = []
+    
+    for m in range(M):
+        if YDists[m] == "Poisson":
+            Y.append(
+                np.column_stack(
+                    [np.random.poisson(np.log(1+np.exp(S[m][:,d])))
+                     for d in range(D[m])]
+                    )            
+                )
+        elif YDists[m] == "Normal":
+            Y.append(
+                np.column_stack(
+                    [np.random.normal(loc = S[m][:,d],
+                                      scale = np.random.uniform(low=YSdRangeNorm[0], high=YSdRangeNorm[1], size=1)
+                                      ) for d in range(D[m])]
+                    )
+                )
+        elif YDists[m] == "Beta":
+            Yalphas = np.random.uniform(low=YaRangeBeta[0], high=YaRangeBeta[1], size=D[m])
+            Y.append(
+                np.column_stack(
+                    [np.random.beta(a = Yalphas[d], 
+                                    b = Yalphas[d] / np.power(1+np.exp(-S[m][:,d]),-1) - Yalphas[d] + 0.1
+                                    ) for d in range(D[m])]
+                    )
+                )
+        elif YDists[m] == "Bernoulli":
+            Y.append(
+                np.column_stack(
+                    [np.random.binomial(n=1,
+                                        p = np.power(1+np.exp(-S[m][:,d]),-1)
+                                        ) for d in range(D[m])]
+                    )            
+                )
+        else: 
+            print("Distributions must be either Poisson, Normal, Beta or Bernoulli")
+            sys.exit()
+            
+    return {'Nc': Nc, 'CIds': CIds, 'IfLrn': IfLrn, 'ZMuC': ZMuC, 'W': W, 'Y': Y}
+
+    ## this returns a dictionary containing the following: 
+    # Nc: The number of samples in each group of samples. The learning dataset groups are first
+    # CIds: The group membership for each sample
+    # IfLrn: Whether a not each sample is in the learning dataset
+    # ZMuC: Matrix containing sample score means by sample group (rows) and factor (column)
+    # W: list of weight matrices, in same order as the likelihoods specified for observed data. Each matrix has features weights by feature (rows) and factor (columns)
+    # Y: list of omics data matrices, in same order as the likelihoods specified for observed data. Each matrix has an observed data point by sample (rows) and feature (column)
+    
+        
---
+++ SimulationStudy/Simulation_Loop.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 15:28:19 2022
+
+@author: david
+"""
+
+# This code imports the function for simulating a multi-omics data set
+# based on the specified number of simulation iterations
+# it simulates multi-omics datasets using the SimMultiOmics function from the imported Multi_Omics_Simulator module
+# for each simulation instance a folder is created and the simulated data is saved in csv files
+# a single json file is saved - it contains metadat for the simulation configuration
+
+import sys
+import os
+import numpy as np
+import json
+
+# set the random seed
+
+seed = 1234567
+np.random.seed(seed)
+
+# set base directory as the current working directory which should be the script's parent folder
+BasePath = os.getcwd()
+
+#  import the module containing the simulation function - expected to be in same folder as this script
+sys.path.append(os.path.join(BasePath))
+import Multi_Omics_Simulator as sm
+
+#####
+###### Set the output folders
+#####
+
+# Set the name of the parent folder that will contain the simulated data for this configuration
+Config = "SimConfig1"
+ConfigDir = os.path.join(BasePath,Config)
+
+# folder name to hold data for each simulation as well as name of output meadata file - THESE DONT NEED TO BE CHANGED   
+SimsDir = os.path.join(ConfigDir,"SimData")   
+sim_meta_out = os.path.join(ConfigDir,"sim_meta.json")
+
+
+#####
+##### set the simulation parameters
+#####
+
+Sims = 2
+
+CLrn = 40 
+CLrnNPool = [10,25,40] 
+CTrg = 2 
+CTrgN = 5
+K = 20 
+ZMuBase = 5
+ZSdRange = [1,1]
+ZMuShift = [-2,2] 
+ZShiftP = 1/4
+
+D = [2000, 2000, 2000] 
+YDists = ["Poisson", "Normal", "Bernoulli"]
+
+
+WThetaRange = [0.15, 0.25]
+WMuPois = 5 
+WSdRangePois = [0.5, 1.5]
+WMuNorm = 0
+WSdRangeNorm = [0.5, 1.5]
+WMuBeta = 0 
+WSdRangeBeta = [0.25, 0.75]
+WMuBern = 0 
+WSdRangeBern = [0.10, 0.20]
+YSdRangeNorm = [0.25, 0.75] 
+YaRangeBeta = [50, 150]
+
+#########
+#### simulate datasets and save to computer
+#########
+
+for s in range(Sims):  
+        
+    # simulate the data using the imported function
+    SimData = sm.SimMultiOmics(
+        CLrn = CLrn, CLrnNPool = CLrnNPool, CTrg = CTrg, CTrgN = CTrgN, 
+        K = K, ZMuBase = ZMuBase, ZSdRange = ZSdRange, ZMuShift = ZMuShift, ZShiftP = ZShiftP, 
+        D = D, YDists = YDists, 
+        WThetaRange = WThetaRange, 
+        WMuPois = WMuPois, WSdRangePois = WSdRangePois, 
+        WMuNorm = WMuNorm, WSdRangeNorm = WSdRangeNorm, 
+        WMuBeta = WMuBeta, WSdRangeBeta = WSdRangeBeta, 
+        WMuBern = WMuBern, WSdRangeBern = WSdRangeBern,
+        YSdRangeNorm = YSdRangeNorm, YaRangeBeta = YaRangeBeta
+        )
+    
+    # create directories to store results in
+    
+    # these root directories only created during first sim
+    
+    if not os.path.exists(ConfigDir):
+        os.mkdir(ConfigDir)
+        
+    if not os.path.exists(SimsDir):
+        os.mkdir(SimsDir)
+        
+    # a results folder for each sim
+    
+    sim_out_dir = os.path.join(SimsDir,"sim" + str(s))
+
+    if not os.path.exists(sim_out_dir):
+        os.mkdir(sim_out_dir)
+    
+    # save simulated data to file
+        
+    np.savetxt(os.path.join(sim_out_dir, 'CIds.csv'), SimData['CIds'], delimiter=',')
+    np.savetxt(os.path.join(sim_out_dir, 'IfLrn.csv'), SimData['IfLrn'], delimiter=',')
+    np.savetxt(os.path.join(sim_out_dir, 'Nc.csv'), SimData['Nc'], delimiter=',')
+    np.savetxt(os.path.join(sim_out_dir, 'ZMuC.csv'), SimData['ZMuC'], delimiter=',')
+    
+    for m in range(len(D)):
+        np.savetxt(os.path.join(sim_out_dir, 'W' + str(m) + '.csv'), SimData['W'][m], delimiter=',')
+        np.savetxt(os.path.join(sim_out_dir, 'Y' + str(m) + '.csv'), SimData['Y'][m], delimiter=',')
+
+#### save the meta data
+
+sim_meta = {
+    'Sims': Sims,
+    'CLrn': CLrn, 'CLrnNPool': CLrnNPool, 'CTrg': CTrg, 'CTrgN': CTrgN, 
+    'K': K, 'ZMuBase': ZMuBase, 'ZSdRange': ZSdRange, 'ZMuShift': ZMuShift, 'ZShiftP': ZShiftP, 
+    'D': D, 'YDists': YDists, 
+    'WThetaRange': WThetaRange, 
+    'WMuPois': WMuPois, 'WSdRangePois': WSdRangePois, 
+    'WMuNorm': WMuNorm, 'WSdRangeNorm': WSdRangeNorm, 
+    'WMuBeta': WMuBeta, 'WSdRangeBeta': WSdRangeBeta,
+    'WMuBern': WMuBern, 'WSdRangeBern': WSdRangeBern,
+    'YSdRangeNorm': YSdRangeNorm, 'YaRangeBeta': YaRangeBeta,
+    'seed': seed
+    }
+
+
+with open(sim_meta_out, "w") as outfile:
+    json.dump(sim_meta,outfile)
+    
+    
+print("simulation loop finished, data saved in " + ConfigDir)    
