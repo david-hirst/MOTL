9299abfa2c27b3382651d1f8ae95ea7a27a1300a
david-hirst <147520988+david-hirst@users.noreply.github.com>
2024-04-17 15:04:02 +0200

    Update 05_TL_VI.R comments

--- TCGAStudy/05_TL_VI.R
+++ TCGAStudy/05_TL_VI.R
@@ -1,6 +1,13 @@
 ## MT - 20240227
 
-## SET FUNCTIONS
+#############
+## APPLY MOTL TO TCGA MULTI-OMICS TARGET DATASETS
+#############
+
+# This script can be used to apply MOTL to the TARGET datasets created from a particular combination of TCGA projects
+# The result will be a factorization of each TARGET dataset
+
+## IMPORT FUNCTIONS
 source("TCGA_preprocessedData_functions.R")
 source("TL_VI_functions.R")
 
@@ -14,16 +21,16 @@ library("dplyr")
 Seed = 1234567
 TopD = 5000
 LrnK = 100
-viewsTrg = c('mRNA','miRNA','DNAme')
-Prjcts = c("TCGA-LAML", "TCGA-SKCM")
-SS_size = 5
-YTrgFull <- list()
+viewsTrg = c('mRNA','miRNA','DNAme') # The omics in the TARGET dataset
+Prjcts = c("TCGA-LAML", "TCGA-SKCM") # The TCGA projects from which the TARGET datasets are created
+SS_size = 5 # The subset size, which is the number of samples per project in each TARGET dataset
+YTrgFull <- list() # placeholder for a multi-omics dataset for all samples that will be subsetted to create TARGET datasets
 
 ## FOLDER PARAMETERS
-DataDir = "DownloadedData_unfltrd/"
+DataDir = "DownloadedData_unfltrd/" # location of downloaded TCGA data
 
-LrnDir = paste0('Lrn_',TopD,'D')
-LrnFctrnDir = file.path(LrnDir,paste0('Fctrzn_',LrnK,'K'))
+LrnDir = paste0('Lrn_',TopD,'D') # When the LEARNING dataset data files are save
+LrnFctrnDir = file.path(LrnDir,paste0('Fctrzn_',LrnK,'K')) # where the LEARNING dataset MOFA factorization is saved
 
 TrgPrjcts <- paste0(substr(Prjcts,6,nchar(Prjcts)), collapse='_')
 TrgBrcd <- paste0('Trg_', TrgPrjcts, "_Full_",TopD,'D/expdat_meta.rds')
@@ -31,7 +38,7 @@ TrgDirSS <- paste0('Trg_', TrgPrjcts, '_SS',SS_size,'_',TopD,'D')
 
 ## TL PARAMETERS
 ## LrnSimple = TRUE ## if TRUE then E[W^2] and E[LnTau] are calculated form E[W] and E[Tau] otherwise imported
-CenterTrg = FALSE # Center Trg with own means or use estimated intercepts?
+CenterTrg = FALSE # Center the TARGET datasets with own featurewise means or use estimated intercepts from the LEARNING dataset factorization?
 
 if (CenterTrg){
   TLDirName = 'TL_VI_C'
@@ -39,7 +46,9 @@ if (CenterTrg){
   TLDirName = 'TL_VI'
 }
 
-# ------------------------------------------- LEARNING SET ----
+##############################################################################
+# ------------------------------------------- IMPORT LEARNING DATASET DATA----
+##############################################################################
 
 script_start_time = Sys.time()
 
@@ -68,30 +77,30 @@ Fctrzn@expectations[["WSq"]] = sapply(viewsLrn, WSq_calculation, Fctrzn, LrnFctr
 ## import the W intercepts if not centering, create vectors containing 0s if centering
 Fctrzn@expectations[["W0"]] = sapply(viewsLrn, W0_calculation, CenterTrg, Fctrzn, LrnFctrnDir)
 
-# -------------------------------------------------------------
-
-# --------------------------------------------- TARGET SET ----
+##############################################################################
+# -----------------------DATA FOR ALL SAMPLES FROM TARGET DATASET PROJECTS----
+##############################################################################
 
 ## harmonize the vectors of views and likelihoods
 views = viewsLrn[is.element(viewsLrn,viewsTrg)]
 likelihoods = likelihoodsLrn[views]
 
-### Import omics data for all Trg set samples
+### Import omics data for all TARGET dataset samples
 YTrgFull_brds <- readRDS(TrgBrcd)
 Prjcts <- unique(YTrgFull_brds$brcds_mRNA$prjct)
 
-## Prepare YTrgFull
+## Prepare YTrgFull, which will be subsetted to create TARGET datasets
 YTrgFull$mRNA <- mRNAdataPreparation(Prjcts = Prjcts, brcds_mRNA = YTrgFull_brds$brcds_mRNA, InputDir = DataDir)
 YTrgFull$miRNA <- miRNAdataPreparation(Prjcts = Prjcts, brcds_miRNA = YTrgFull_brds$brcds_miRNA, InputDir = DataDir)
 YTrgFull$DNAme <- DNAmedataPreparation(Prjcts = Prjcts, brcds_DNAme = YTrgFull_brds$brcds_DNAme, InputDir = DataDir)
 # YTrgFull$SNV <- SNVdataPreparation(Prjcts = Prjcts, brcds_SNV = YTrgFull_brds$brcds_SNV, InputDir = DataDir)
 
-# -------------------------------------------------------------
-
-# -------------------------------------- TRANSFER LEARNING ----
+##############################################################################
+# --------------------CREATE AND PREPROCESS TARGET DATASETS AND APPLY MOTL----
+##############################################################################
 
 ## PARAMETERS
-minFactors = 15 ## floor when dropping factors - number of samples in evlauations
+minFactors = 10 ## floor when dropping factors - we used the number of samples
 StartDropFactor = 1 # after which iteration to start dropping factors
 FreqDropFactor = 1 ## how often to drop factors
 StartELBO = 1 #which iteration to start checking ELBO on, excl initiation iteration
@@ -103,12 +112,13 @@ ConvergenceIts = 2 # as per MOFA python defaults
 ConvergenceTH = 0.0005 # default for MOFA - fast option
 
 ## import predefined subset information
+## these are the samples already allocated to each TARGET dataset 
 brcds_SS = readRDS(file.path(TrgDirSS,'brcds_SS.rds'))
 
-## loop to subset, preprocess, perform TL and export
+## loop to subset YTrgFull to create, pre-process and apply MOTL to TARGET datsets
 loop_start_time = Sys.time()
 
-## FOR EACH TARGET SUBSET
+## FOR EACH TARGET DATASET
 for(ss in 1:brcds_SS$SS_count){
   
   ss_start_time = Sys.time()
@@ -122,10 +132,10 @@ for(ss in 1:brcds_SS$SS_count){
     dir.create(TL_SSOutDir, showWarnings = FALSE, recursive = TRUE)
   }
   
-  ## extract sample ids for the subset
+  ## extract sample ids for the TARGET dataset
   smpls = brcds_SS$smpls_SS[[SS]]
   
-  ## Prepare YTrg subset data 
+  ## select a subset of YTrgFull to create the TARGET dataset
   YTrgSS = TCGATargetDataPreparation(views = views, YTrgFull = YTrgFull, brcds_SS = brcds_SS, 
                                      SS = SS, Fctrzn = Fctrzn, smpls = smpls, expdat_meta_Lrn = expdat_meta_Lrn)
   
@@ -133,7 +143,7 @@ for(ss in 1:brcds_SS$SS_count){
   TL_param = initTransferLearningParamaters(views = views, YTrg = YTrgSS, Fctrzn = Fctrzn, 
                                             likelihoods = likelihoods, expdat_meta_Lrn = expdat_meta_Lrn)
   
-  ## TRANSFER LEARNING
+  ## TRANSFER LEARNING WITH MOTL
   TL_output <- transferLearning_function(TL_param = TL_param, MaxIterations= MaxIterations, MinIterations = MinIterations, minFactors = minFactors, 
                             StartDropFactor = StartDropFactor, FreqDropFactor = FreqDropFactor, StartELBO = StartELBO, 
                             FreqELBO = FreqELBO, DropFactorTH = DropFactorTH, ConvergenceIts = ConvergenceIts, ConvergenceTH = ConvergenceTH, 
