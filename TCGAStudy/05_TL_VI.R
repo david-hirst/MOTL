ba51d18c0018bce6be34caecf631bee7dab704b2
MOohTus <caco630@hotmail.fr>
2024-05-24 15:09:54 +0200

    Add intercept calling in Glio workflow

--- TCGAStudy/05_TL_VI.R
+++ TCGAStudy/05_TL_VI.R
@@ -75,7 +75,7 @@ Fctrzn@expectations[["TauLn"]] = sapply(viewsLrn, TauLn_calculation, likelihoods
 Fctrzn@expectations[["WSq"]] = sapply(viewsLrn, WSq_calculation, Fctrzn, LrnFctrnDir)
 
 ## Calculate the intercepts from the factorization
-intercepts_calculation(Seed, expdat_meta_Lrn, Fctrzn, FctrznDir = ".", LrnDir)
+intercepts_calculation(Seed, expdat_meta_Lrn, Fctrzn, LrnFctrnDir, LrnDir)
 
 ## import the W intercepts if not centering, create vectors containing 0s if centering
 Fctrzn@expectations[["W0"]] = sapply(viewsLrn, W0_calculation, CenterTrg, Fctrzn, LrnFctrnDir)
